066fb420b83cb102bfde97957ed16b0d61c67347
TheFreshPenguin <31851170+TheFreshPenguin@users.noreply.github.com>
2018-12-17 01:06:59 +0100

    get and post according to ids

--- src/metrics.ts
+++ src/metrics.ts
@@ -18,22 +18,44 @@ export class MetricsHandler {
     this.db = LevelDb.open(dbPath)
   }
 
-  static get(callback: (error: Error | null, result?: Metric[]) => void) {
-    const result = [
-      new Metric('2013-11-04 14:00 UTC', 12),
-      new Metric('2013-11-04 14:30 UTC', 15)
-    ]
-    callback(null, result)
+
+// public get(callback: (error: Error | null, result?: Metric[]) => void) {
+//     const result = [
+//       new Metric('2013-11-04 14:00 UTC', 12),
+//       new Metric('2013-11-04 14:30 UTC', 15)
+//     ]
+//     callback(null, result)
+//   }
+
+public get(key: string, callback: (err: Error | null, result?: Metric[]) => void) {
+    const stream = this.db.createReadStream()
+    var met: Metric[] = []
+
+    stream.on('error', callback)
+      .on('end', (err: Error) => {
+        callback(null, met)
+      })
+      .on('data', (data: any) => {
+        const [_, k, timestamp] = data.key.split(":")
+        console.log(data.key+"\n")
+        const value = data.value
+
+        if (key != k) {
+          console.log(`LevelDB error: ${data} does not match key ${key}`)
+        } else {
+          met.push(new Metric(timestamp, value))
+        }
+      })
   }
 
-  public save(key: number, metrics: Metric[], callback: (error: Error | null) => void) {
-  const stream = WriteStream(this.db)
-  stream.on('error', callback)
-  stream.on('close', callback)
-  metrics.forEach((m: Metric) => {
-    stream.write({ key: `metric:${key}${m.timestamp}`, value: m.value })
-  })
-  stream.end()
+  public save(key: string, metrics: Metric[], callback: (error: Error | null) => void) {
+    const stream = WriteStream(this.db)
+    stream.on('error', callback)
+    stream.on('close', callback)
+    metrics.forEach((m: Metric) => {
+      stream.write({ key: `metric:${key}:${m.timestamp}`, value: m.value })
+    })
+    stream.end()
   }
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,47 @@
 import express = require('express')
-import { MetricsHandler } from './metrics'
+import { MetricsHandler, Metric } from './metrics'
 
 const app = express()
 const port: string = process.env.PORT || '8080'
+
+var bodyParser = require('body-parser')
+
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 app.get('/', (req: any, res: any) => {
   res.write('Hello world')
   res.end()
 })
 
-app.get('/metrics', (req: any, res: any) => {
-  MetricsHandler.get((err: Error | null, result?: any) => {
-    if (err) {
-      throw err
-    }
-    res.json(result)
+// app.get('/metrics', (req: any, res: any) => {
+//   // console.log(MetricsHandler.get)
+//   dbMet.get((err: Error | null, result?: any) => {
+//     if (err) {
+//       throw err
+//     }
+//     res.json(result)
+//   })
+// })
+
+app.get('/:id', (req: any, res: any, next: any) => {
+  dbMet.get(req.params.id, (err: Error | null, result?: Metric[]) => {
+    if (err) next(err)
+    if (result === undefined) {
+      res.write('no result')
+      res.send()
+    } else res.json(result)
   })
 })
+
+app.post('/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
+    if (err) throw (err)
+    res.status(200).send()
+  })
+});
 
 app.listen(port, (err: Error) => {
   if (err) {

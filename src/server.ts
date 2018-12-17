7f575084618e3b0ad6bd5cee60648295d0f88eaa
TheFreshPenguin <31851170+TheFreshPenguin@users.noreply.github.com>
2018-12-17 06:20:40 +0100

    adding auth strats

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express = require('express')
 import { MetricsHandler, Metric } from './metrics'
+import { UserHandler, User } from './users'
+import session = require('express-session')
+import levelSession = require('level-session-store')
 
 const app = express()
 const port: string = process.env.PORT || '8080'
@@ -7,24 +10,95 @@ const port: string = process.env.PORT || '8080'
 var bodyParser = require('body-parser')
 
 const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+const LevelStore = levelSession(session)
+
+const dbUser: UserHandler = new UserHandler('./db/users')
+const authRouter = express.Router()
+const userRouter = express.Router()
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
+
+app.use(session({
+  secret: 'my very secret phrase',
+  store: new LevelStore('./db/sessions'),
+  resave: true,
+  saveUninitialized: true
+}))
+
+authRouter.get('/login', (req: any, res: any) => {
+  res.render('login')
+})
+
+authRouter.get('/signup', (req: any, res: any) => {
+  res.render('signup')
+})
+
+authRouter.get('/logout', (req: any, res: any) => {
+  delete req.session.loggedIn
+  delete req.session.user
+  res.redirect('/login')
+})
+
+app.post('/login', (req: any, res: any, next: any) => {
+  dbUser.get(req.body.username, (err: Error | null, result?: User) => {
+    if (err) next(err)
+    if (result === undefined || !result.validatePassword(req.body.password)) {
+      res.redirect('/login')
+    } else {
+      req.session.loggedIn = true
+      req.session.user = result
+      res.redirect('/')
+    }
+  })
+})
+
+app.use(authRouter)
+
+userRouter.post('/', (req: any, res: any, next: any) => {
+  dbUser.get(req.body.username, function (err: Error | null, result?: User) {
+    if (!err || result !== undefined) {
+      res.status(409).send("user already exists")
+    } else {
+      dbUser.save(req.body, function (err: Error | null) {
+
+        if (err) next(err)
+
+        else res.status(201).send("user persisted")
+      })
+    }
+  })
+})
+
+userRouter.get('/:username', (req: any, res: any, next: any) => {
+  dbUser.get(req.params.username, function (err: Error | null, result?: User) {
+    if (err || result === undefined) {
+      res.status(404).send("user not found")
+    } else res.status(200).json(result)
+  })
+})
+
+app.use('/user', userRouter)
+
+
+const authCheck = function (req: any, res: any, next: any) {
+  if (req.session.loggedIn) {
+    next()
+  } else res.redirect('/login')
+}
+
+app.get('/', authCheck, (req: any, res: any) => {
+  res.render('index', { name: req.session.username })
+})
 
 app.get('/', (req: any, res: any) => {
   res.write('Hello world')
   res.end()
 })
 
-// app.get('/metrics', (req: any, res: any) => {
-//   // console.log(MetricsHandler.get)
-//   dbMet.get((err: Error | null, result?: any) => {
-//     if (err) {
-//       throw err
-//     }
-//     res.json(result)
-//   })
-// })
 
 app.get('/:id', (req: any, res: any, next: any) => {
   dbMet.get(req.params.id, (err: Error | null, result?: Metric[]) => {
@@ -42,6 +116,8 @@ app.post('/:id', (req: any, res: any) => {
     res.status(200).send()
   })
 });
+
+
 
 app.listen(port, (err: Error) => {
   if (err) {
